2684855f31d99d0e7c81bc8b2a06a21d43512118
thejeffreyallen <Thejeffreyallen@gmail.com>
2020-04-27 16:55:28 -0600

    Added BTree insert methods
    I also made appropriate changes to the BTreeNode class

--- src/BTree.java
+++ src/BTree.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 
 /**
@@ -8,10 +9,11 @@ import java.util.Collections;
  */
 public class BTree {
 
-	private BTreeNode root;
-	private int height, degree, seqLength, debugLevel, cacheSize;
+	private BTreeNode root, parent, leftSibling, rightSibling; // Project instructions suggest keeping a few more nodes in memory
+	private int height, degree, seqLength, debugLevel, cacheSize, nodeCount;
 	private String fileName;
-	//TODO - Add unimplemented variables
+	private BTreeRW rw;
+	// TODO - Add unimplemented variables
 
 	/**
 	 * BTree constructor - initializes a new BTree object for writing to file
@@ -32,13 +34,14 @@ public class BTree {
 	 * 
 	 */
 	public BTree(int degree, String fileName, int seqLength, int cacheSize, int debugLevel) {
-		root = new BTreeNode(0, degree, true, true); // index = 0, degree, isRoot, isLeaf
+		root = new BTreeNode(nodeCount++, degree, true, true); // index = 0, degree, isRoot, isLeaf
 		this.degree = degree;
 		this.fileName = fileName;
 		this.seqLength = seqLength;
 		this.cacheSize = cacheSize;
 		this.debugLevel = debugLevel;
-		//TODO - Add unimplemented code
+		rw = new BTreeRW(fileName, cacheSize);
+		// TODO - Add unimplemented code
 	}
 
 	/**
@@ -64,23 +67,68 @@ public class BTree {
 		this.seqLength = seqLength;
 		this.cacheSize = cacheSize;
 		this.debugLevel = debugLevel;
-		//TODO - Add unimplemented code
+		rw = new BTreeRW(fileName, cacheSize);
+		// TODO - Add unimplemented code
 	}
 
 	/**
 	 * 
 	 * @param o - TreeObject to insert
 	 */
-	public void insert(TreeObject o) {
-		//TODO - Add unimplemented method
+	public void insert(TreeObject k) {
+		// TODO - Add unimplemented method
+		if (this.root == null) // if tree is initially empty
+		{
+			this.root = new BTreeNode(nodeCount++, this.degree, true, false); // Allocate new node as root
+			this.root.addKey(k); // key added, done
+		} else// if tree is not empty
+		{
+			if (this.root.isFull()) {
+				BTreeNode s = new BTreeNode(nodeCount++, this.degree, true, false); // Allocate new node
+				s.addChild(root.getIndex()); // make the current root a child of s
+				root.setParentPointer(s.getIndex());
+				splitChild(s, root.getIndex(), root); // split the node "root"
+				// TODO find correct child node to insert not full
+				BTreeNode child = null; // need to set to correct child node
+				insertNotFull(child, k);
+
+				root = s; // make s the new root
+
+			} else {
+				insertNotFull(root, k);
+			}
+		}
 	}
 
 	/**
 	 * 
-	 * @param o - TreeObject to insert
+	 * @param x - Node to enter key value in
+	 * @param k - Key value to enter into node x
 	 */
-	public void insertNotFull(TreeObject o) {
-		//TODO - Add unimplemented method
+	public void insertNotFull(BTreeNode x, TreeObject k) {
+		// TODO - Add unimplemented method
+		int i = x.getKeyCount(); // start at the right most key in node x
+		if (x.isLeaf()) {
+			while (i >= 0 && k.compareTo(x.getKey(i)) == -1) // find the correct position to insert k
+			{
+				i--;
+			}
+			x.insertKey(i, k); // insert k at index i
+			rw.diskWrite(x); // write to disk, done.
+		} else {
+			while (i >= 1 && k.compareTo(x.getKey(i)) == -1) { // find the correct position to insert k
+				i--; // line 10 - in class btree pseudocode
+			}
+			i++;// line 11 - in class btree pseudocode
+			BTreeNode c = rw.diskRead(x.getChildPointer(i)); // read child node from disk at offset i
+			if (c.getKeyCount() == (2 * degree) - 1) { // if node is full
+				splitChild(x, i, c); // split node
+				if (k.compareTo(x.getKey(i)) == -1) { // find the correct position to insert k
+					i++;// line 16 - in class btree pseudocode
+				}
+				insertNotFull(c, k);
+			}
+		}
 	}
 
 	/**
@@ -97,15 +145,15 @@ public class BTree {
 
 		x.setParentPointer(y.getParentPointer());
 
-		// add y's node's second  half to z node and reindex
+		// add y's node's second half to z node and reindex
 		for (int j = 0; j < degree - 1; j++) {
-			// add y's node's second  half to z node and reindex
+			// add y's node's second half to z node and reindex
 			z.setKey(j, y.getKey(j + degree));
 			// update number of keys for z and y
 			z.increaseKeyCount();
 			y.decreaseKeyCount();
 		}
-		//checking if y is a leaf
+		// checking if y is a leaf
 		if (y.isLeaf() != true) {
 
 			// reindexing y's childern to z's childen
@@ -117,13 +165,13 @@ public class BTree {
 		}
 		// n[y] = t-1
 		y.setKeyCount(degree - 1);
-		//10 for loop
+		// 10 for loop
 		for (int j = x.getKeyCount(); j > y.getKeyCount(); j++) {
 			x.setChildPointer(j + 1, x.getChildPointer(j));
 		}
 		x.setChildPointer(i + 1, z.getIndex());
 
-		//13 for loop
+		// 13 for loop
 		for (int j = x.getKeyCount() - 1; j > y.getKeyCount() - 1; j++) {
 			x.setKey(j + 1, x.getKey(j));
 		}
